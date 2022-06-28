c4edf3f5af951fc82a0c5e909100dc1d055a98f2
Dali <oscardali370@gmail.com>
2022-06-28 16:09:58 -0500

    Fix location and main class

--- Service/src/main/java/me/upp/daligz/service/Service.java
+++ Service/src/main/java/me/upp/daligz/service/Service.java
@@ -10,15 +10,10 @@ public class Service {
     public static void main(final String[] args) {
         final Connector connector = new Connector();
         Spark.port(6969);
-        Spark.get("/images/:amount/:category", (request, response) -> {
-//            final GetImages getImages = new GetImages(Integer.parseInt(request.params(":amount")), request.params(":category"), connector);
-//            final String get = getImages.get();
-            final String posts = new PostStructure(connector.getMySQL()).getPosts(request.params(":category"), Integer.parseInt(request.params(":amount")));
-            System.out.println(request.toString());
-            System.out.println(response.toString());
-            System.out.println(posts);
-            return posts;
-        });
+
+        // Posts
+        Spark.get("/posts/get/:amount/:category", (request, response) -> new PostStructure(connector.getMySQL())
+                .getPosts(request.params(":category"), Integer.parseInt(request.params(":amount"))));
 
         // Users
         Spark.get("/users/insert/:mac", (request, response) -> {
