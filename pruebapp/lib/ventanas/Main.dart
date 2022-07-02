3b8c588f8e86584c241c50469bb4b8a78c06b50f
Yoana2000 <yobiz2000@gmail.com>
2022-07-02 17:26:26 -0500

    Agregar botones de clasificacion deportes/comida

--- pruebapp/lib/ventanas/Main.dart
+++ pruebapp/lib/ventanas/Main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:pruebapp/ventanas/favoritos.dart';
 import 'package:scroll_app_bar/scroll_app_bar.dart';
 
@@ -8,7 +9,9 @@ class Main extends StatelessWidget {
   final controller = ScrollController();
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+    return DefaultTabController(
+      length: 2,
+        child: Scaffold(
       appBar: ScrollAppBar(
         controller: controller,
         backgroundColor: Color(0XFFA96DBB),
@@ -18,38 +21,62 @@ class Main extends StatelessWidget {
             style: TextStyle(
               color: Color(0XFF000000),
               fontWeight: FontWeight.bold,
-              fontSize: 30,
             ),
         ),
         actions: [
           Row(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100)
-                   ),
-              child: RaisedButton(
-                child:  IconButton(
-                  iconSize: 40.0,
-              icon: Icon(
-                  FontAwesomeIcons.heartCircleCheck,
-                  color: Colors.black,
-                  ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SegundaPagina()));
-                },
-                  ),
-                onPressed: (){
-
-                },
-              ),
-              ),
+              _accionesApp(context),
             ],
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child:  Column(
+          children: [
+            TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(
+                    Ionicons.american_football,
+                    color: Colors.black,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Ionicons.fast_food,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+        ),
     );
   }
 }
 
+Widget _accionesApp(context){
+  return Container(
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100)
+    ),
+    child: RaisedButton(
+      child:  IconButton(
+        iconSize: 40.0,
+        icon: Icon(
+          FontAwesomeIcons.heartCircleCheck,
+          color: Colors.black,
+        ),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> SegundaPagina()));
+        },
+      ),
+      onPressed: (){
+      },
+    ),
+  );
+}
 
--- pruebapp/lib/ventanas/favoritos.dart
+++ pruebapp/lib/ventanas/favoritos.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:scroll_app_bar/scroll_app_bar.dart';
 
 class SegundaPagina extends StatelessWidget {
+  final controller = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Favoritos'),),
-      body: Center(
-
+      appBar: ScrollAppBar(
+        controller: controller,
+        backgroundColor: Color(0XFFA96DBB),
+        elevation: 0,
+        title: Text(
+          'Favoritos',
+          style: TextStyle(
+            color: Color(0XFF000000),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
