e0a0680d3c25eb77f7c4a80f14eeb364a1174020
Dali <oscardali370@gmail.com>
2022-07-08 23:25:25 -0500

    Favorites routes implemented and fix users routes

--- pruebapp/lib/service/service_routes.dart
+++ pruebapp/lib/service/service_routes.dart
@@ -7,6 +7,12 @@ class Routes {
   static String routePostGet(final String category, final int amount) => url + "/posts/get/$amount/$category";
   static String routePostGetAll(final int amount) => url + "/posts/get/$amount";
 
-  // Users favorites
-  static String routeUsersInsert(final String mac) => url + "/users/insert/$mac";
+  // Users routes
+  static String routeUserInsert(final String mac) => url + "/users/insert/$mac";
+
+  // Favorites routes
+  static String routeFavoriteInsert(final String mac, final String postId) => url + "/fav/insert/$mac/$postId";
+  static String routeFavoriteDelete(final String mac, final String postId) => url + "/fav/delete/$mac/$postId";
+  static String routeFavoriteGet(final String mac, final String postId) => url + "/fav/get/$mac/$postId";
+  static String routeFavoriteGetAll(final String mac) => url + "/fav/get/$mac";
 }
