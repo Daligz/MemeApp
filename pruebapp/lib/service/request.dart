2ef5585c17339c99bdc2003a678b310f32b22c4a
Dali <oscardali370@gmail.com>
2022-07-08 23:39:41 -0500

    http dependency updated and register device test pre-implemente

--- pruebapp/lib/service/request.dart
+++ pruebapp/lib/service/request.dart
@@ -1,1 +1,10 @@
-class Service { }
+import 'package:http/http.dart';
+import 'package:pruebapp/service/service_routes.dart';
+
+class Service {
+
+  Future<bool> registerDevice() async {
+    final Response response = await get(Uri.parse(Routes.routeUserInsert("test")));
+    return (response.statusCode == 200);
+  }
+}
