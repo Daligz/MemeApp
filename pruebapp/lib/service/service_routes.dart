5894abf1afaf75dd82e20de1e006995ced09f04a
Dali <oscardali370@gmail.com>
2022-07-08 23:22:18 -0500

    Users routes implemented

--- pruebapp/lib/service/service_routes.dart
+++ pruebapp/lib/service/service_routes.dart
@@ -6,4 +6,7 @@ class Routes {
   // Posts routes
   static String routePostGet(final String category, final int amount) => url + "/posts/get/$amount/$category";
   static String routePostGetAll(final int amount) => url + "/posts/get/$amount";
+
+  // Users favorites
+  static String routeUsersInsert(final String mac) => url + "/users/insert/$mac";
 }
