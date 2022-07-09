ed3449af414f287d6d4e2a9ffe7044225fc4c6e4
Dali <oscardali370@gmail.com>
2022-07-08 23:21:57 -0500

    Posts routes implemented

--- pruebapp/lib/service/service_routes.dart
+++ pruebapp/lib/service/service_routes.dart
@@ -1,3 +1,9 @@
 class Routes {
+
+  // Main url for the connection to the web service
   static const String url = "localhost:6969";
+
+  // Posts routes
+  static String routePostGet(final String category, final int amount) => url + "/posts/get/$amount/$category";
+  static String routePostGetAll(final int amount) => url + "/posts/get/$amount";
 }
