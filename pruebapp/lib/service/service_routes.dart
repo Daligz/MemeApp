d77aae98731a2c03abe0b5168a011e8e87b52f7b
Dali <oscardali370@gmail.com>
2022-07-08 23:29:35 -0500

    url set to private

--- pruebapp/lib/service/service_routes.dart
+++ pruebapp/lib/service/service_routes.dart
@@ -1,18 +1,18 @@
 class Routes {
 
-  // Main url for the connection to the web service
-  static const String url = "localhost:6969";
+  // Main _url for the connection to the web service
+  static const String _url = "localhost:6969";
 
   // Posts routes
-  static String routePostGet(final String category, final int amount) => url + "/posts/get/$amount/$category";
-  static String routePostGetAll(final int amount) => url + "/posts/get/$amount";
+  static String routePostGet(final String category, final int amount) => _url + "/posts/get/$amount/$category";
+  static String routePostGetAll(final int amount) => _url + "/posts/get/$amount";
 
   // Users routes
-  static String routeUserInsert(final String mac) => url + "/users/insert/$mac";
+  static String routeUserInsert(final String mac) => _url + "/users/insert/$mac";
 
   // Favorites routes
-  static String routeFavoriteInsert(final String mac, final String postId) => url + "/fav/insert/$mac/$postId";
-  static String routeFavoriteDelete(final String mac, final String postId) => url + "/fav/delete/$mac/$postId";
-  static String routeFavoriteGet(final String mac, final String postId) => url + "/fav/get/$mac/$postId";
-  static String routeFavoriteGetAll(final String mac) => url + "/fav/get/$mac";
+  static String routeFavoriteInsert(final String mac, final String postId) => _url + "/fav/insert/$mac/$postId";
+  static String routeFavoriteDelete(final String mac, final String postId) => _url + "/fav/delete/$mac/$postId";
+  static String routeFavoriteGet(final String mac, final String postId) => _url + "/fav/get/$mac/$postId";
+  static String routeFavoriteGetAll(final String mac) => _url + "/fav/get/$mac";
 }
