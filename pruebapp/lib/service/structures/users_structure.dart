1178e09577736533ebada5acebc031df1d62e7cd
Dali <oscardali370@gmail.com>
2022-07-10 22:50:35 -0500

    Exists implemented and added to insert method

--- pruebapp/lib/service/structures/users_structure.dart
+++ pruebapp/lib/service/structures/users_structure.dart
@@ -8,7 +8,13 @@ class UserStructure {
   Future<bool> insert() async {
     final String macAddress = await Device.getMacAddress();
     if (macAddress.isEmpty) return false;
+    if (await exists(macAddress)) return false;
     final Response response = await get(Uri.parse(Routes.routeUserInsert(macAddress)));
     return (response.statusCode == 200);
+  }
+
+  Future<bool> exists(final String macAddress) async {
+    final Response response = await get(Uri.parse(Routes.routeUserExists(macAddress)));
+    return (response.body.toLowerCase() == "true");
   }
 }
