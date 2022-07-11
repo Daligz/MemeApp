9d28d29773ed99666a126be6000fe66bf9823091
Dali <oscardali370@gmail.com>
2022-07-10 23:27:15 -0500

    Get implemented with multiple post structure

--- pruebapp/lib/service/structures/posts_structure.dart
+++ pruebapp/lib/service/structures/posts_structure.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:pruebapp/service/commons/post.dart';
 
@@ -8,7 +10,7 @@ class PostStructure {
   Future<List<Post>> get(final String category, final int amount) async {
     final http.Response response = await http.get(Uri.parse(Routes.routePostGet(category, amount)));
     if (response.statusCode != 200) return List.empty();
-    return List.empty();
+    return (json.decode(response.body) as List).map((e) => Post.fromJson(e)).toList();
   }
 
   Future<String> getRandom(final int amount) async {
