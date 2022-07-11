c07bb0c48f30958121f23c029b35cce6d536769b
Dali <oscardali370@gmail.com>
2022-07-10 23:14:53 -0500

    Check response added and get random method parameter fix

--- pruebapp/lib/service/structures/posts_structure.dart
+++ pruebapp/lib/service/structures/posts_structure.dart
@@ -1,15 +1,17 @@
-import 'package:http/http.dart';
-import 'package:pruebapp/service/utils/device.dart';
+import 'package:http/http.dart' as http;
+import 'package:pruebapp/service/commons/post.dart';
 
 import '../service_routes.dart';
 
 class PostStructure {
 
-  Future<String> get(final String category, final int amount) async {
-    return "";
+  Future<List<Post>> get(final String category, final int amount) async {
+    final http.Response response = await http.get(Uri.parse(Routes.routePostGet(category, amount)));
+    if (response.statusCode != 200) return List.empty();
+    return List.empty();
   }
 
-  Future<String> getRandom(final String amount) async {
+  Future<String> getRandom(final int amount) async {
     return "";
   }
 }
