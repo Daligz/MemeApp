bedb431d3816a3efb4f00a26f4aa216ae7ffef39
Dali <oscardali370@gmail.com>
2022-07-10 22:48:42 -0500

    Device class fix and insert pre-implemented

--- pruebapp/lib/service/commons/device.dart
+++ pruebapp/lib/service/commons/device.dart
@@ -2,7 +2,7 @@ import 'package:platform_device_id/platform_device_id.dart';
 
 class Device {
 
-  static Future<String> getMac() async {
+  static Future<String> getMacAddress() async {
     String? macAddress;
     try {
       macAddress = await PlatformDeviceId.getDeviceId;
--- pruebapp/lib/service/structures/users_structure.dart
+++ pruebapp/lib/service/structures/users_structure.dart
@@ -1,6 +1,14 @@
+import 'package:http/http.dart';
+import 'package:pruebapp/service/commons/device.dart';
+
+import '../service_routes.dart';
+
 class UserStructure {
 
   Future<bool> insert() async {
-    return null;
+    final String macAddress = await Device.getMacAddress();
+    if (macAddress.isEmpty) return false;
+    final Response response = await get(Uri.parse(Routes.routeUserInsert(macAddress)));
+    return (response.statusCode == 200);
   }
 }
