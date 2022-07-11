916d35fe3b98964b0bb574e955289806d1abf0fc
Dali <oscardali370@gmail.com>
2022-07-10 23:38:23 -0500

    Random posts implemented

--- pruebapp/lib/service/structures/posts_structure.dart
+++ pruebapp/lib/service/structures/posts_structure.dart
@@ -13,7 +13,11 @@ class PostStructure {
     return (json.decode(response.body) as List).map((e) => Post.fromJson(e)).toList();
   }
 
-  Future<String> getRandom(final int amount) async {
-    return "";
+  Future<List<Post>> getRandom(final int amount) async {
+    final http.Response response = await http.get(Uri.parse(Routes.routePostGetAll(amount)));
+    if (response.statusCode != 200) return List.empty();
+    final List<Post> posts = (json.decode(response.body) as List).map((e) => Post.fromJson(e)).toList();
+    posts.shuffle();
+    return posts.getRange(0, amount).toList();
   }
 }
