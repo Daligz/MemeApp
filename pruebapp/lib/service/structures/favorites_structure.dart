f61769fadeaee00c7a1592c3f89926a41d93d538
Dali <oscardali370@gmail.com>
2022-07-11 18:26:47 -0500

    Favorite common added and get implemented

--- pruebapp/lib/service/structures/favorites_structure.dart
+++ pruebapp/lib/service/structures/favorites_structure.dart
@@ -1,5 +1,8 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:pruebapp/service/commons/favorite.dart';
 import 'package:pruebapp/service/utils/device.dart';
 
 import '../service_routes.dart';
@@ -20,6 +23,12 @@ class FavoriteStructure {
   Future<bool> delete(final String postId) async {
     final String macAddress = await Device.getMacAddress();
     return commonRequest(Routes.routeFavoriteDelete(macAddress, postId));
+  }
+
+  Future<Favorite> get(final String postId) async {
+    final String macAddress = await Device.getMacAddress();
+    final http.Response response = await http.get(Uri.parse(Routes.routeFavoriteGet(macAddress, postId)));
+    return Favorite.fromJson(json.decode(response.body));
   }
 
   Future<bool> commonRequest(final String route) async {
