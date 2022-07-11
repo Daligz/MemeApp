11e273fb92d113862c9e9ec7e328da02bab8a77f
Dali <oscardali370@gmail.com>
2022-07-11 18:01:03 -0500

    Insert structure implemented to favorite

--- pruebapp/lib/service/structures/favorites_structure.dart
+++ pruebapp/lib/service/structures/favorites_structure.dart
@@ -1,7 +1,14 @@
-import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:pruebapp/service/utils/device.dart';
 
 import '../service_routes.dart';
 
-class FavoriteStructure { }
+class FavoriteStructure {
+
+  Future<bool> insert(final String postId) async {
+    final String macAddress = await Device.getMacAddress();
+    final http.Response response = await http.get(Uri.parse(Routes.routeFavoriteInsert(macAddress, postId)));
+    return (response.body.toLowerCase() == "true");
+  }
+}
