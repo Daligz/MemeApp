2cfd4e16e778d9dccab5e65ee1343c227bce9e0f
Dali <oscardali370@gmail.com>
2022-07-10 22:54:41 -0500

    Post structure pre-implemented

---
+++ pruebapp/lib/service/structures/posts_structure.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart';
+import 'package:pruebapp/service/commons/device.dart';
+
+import '../service_routes.dart';
+
+class PostStructure {
+
+  Future<String> get(final String category, final int amount) async {
+    return "";
+  }
+
+  Future<String> getRandom(final String amount) async {
+    return "";
+  }
+}
