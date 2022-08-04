312749ba2d77ef440359cad5315be32c65156077
Dali <oscardali370@gmail.com>
2022-08-04 15:37:46 -0500

    Dashboard design added

--- AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
@@ -1,10 +1,19 @@
 package me.upp.daligz.admindashboard;
 
 import me.upp.daligz.admindashboard.database.Connector;
+import me.upp.daligz.admindashboard.view.MainView;
+
+import javax.swing.*;
 
 public class AdminDashboard {
 
     public static void main(final String[] args) {
         final Connector connector = new Connector();
+        final JFrame jFrame = new JFrame("Panel de Administrador");
+        jFrame.setContentPane(new MainView().getRootPanel());
+        jFrame.pack();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setBounds(700, 250, 500, 250);
+        jFrame.setVisible(true);
     }
 }
---
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/view/MainView.java
@@ -0,0 +1,18 @@
+package me.upp.daligz.admindashboard.view;
+
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
+public class MainView {
+    private JTextField txtUrl;
+    private JTextField txtCategory;
+    private JPanel imagenPanel;
+    private JPanel rootPanel;
+    private JButton btnCreate;
+    private JButton btnGet;
+    private JButton btnDelete;
+    private JButton btnUpdate;
+    private JButton btnCancel;
+}
