75c008a5f2c779b25136b83696883055b41a4a93
Dali <oscardali370@gmail.com>
2022-08-04 15:47:39 -0500

    Try to load image :c

--- AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
@@ -4,16 +4,21 @@ import me.upp.daligz.admindashboard.database.Connector;
 import me.upp.daligz.admindashboard.view.MainView;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class AdminDashboard {
 
     public static void main(final String[] args) {
         final Connector connector = new Connector();
         final JFrame jFrame = new JFrame("Panel de Administrador");
-        jFrame.setContentPane(new MainView().getRootPanel());
+        final MainView mainView = new MainView();
+        jFrame.setContentPane(mainView.getRootPanel());
         jFrame.pack();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setBounds(700, 250, 500, 250);
         jFrame.setVisible(true);
+        final Image image = Toolkit.getDefaultToolkit().createImage("https://i.pinimg.com/originals/42/c2/61/42c261ecd1f3cb4a0b02185acb053095.gif");
+        mainView.getImagenPanel().prepareImage(image, (img, infoflags, x, y, width, height) -> false);
     }
 }
