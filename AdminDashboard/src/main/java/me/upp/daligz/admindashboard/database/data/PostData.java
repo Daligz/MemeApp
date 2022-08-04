4cea9d9060981a7c7d98532c68d9f8fd33696cbc
Dali <oscardali370@gmail.com>
2022-08-04 11:25:23 -0500

    Delete implemented

--- AdminDashboard/src/main/java/me/upp/daligz/admindashboard/database/data/PostData.java
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/database/data/PostData.java
@@ -3,6 +3,7 @@ package me.upp.daligz.admindashboard.database.data;
 import lombok.AllArgsConstructor;
 import me.upp.daligz.admindashboard.database.Connector;
 import me.upp.daligz.admindashboard.database.tables.PostTable;
+import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.queries.DeleteQuery;
 import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.queries.InsertQuery;
 import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.queries.Query;
 
@@ -12,11 +13,18 @@ public class PostData {
     private final Connector connector;
 
     public void insert(final String url, final String category) {
-        final String insertQuery = new InsertQuery("posts")
+        final String insertQuery = new InsertQuery(PostTable.TABLE_NAME.getValue())
                 .value(PostTable.ID.getValue(), null)
                 .value(PostTable.URL.getValue(), url)
                 .value(PostTable.CATEGORY.getValue(), category)
                 .build();
-        new Query(this.connector.getMySQL(), insertQuery);
+        new Query(this.connector.getMySQL(), insertQuery).executeUpdateAsync();
+    }
+
+    public void delete(final int id) {
+        final String deleteQuery = new DeleteQuery(PostTable.TABLE_NAME.getValue())
+                .where(String.format("'%s' = %s", PostTable.ID.getValue(), id))
+                .build();
+        new Query(this.connector.getMySQL(), deleteQuery).executeUpdateAsync();
     }
 }
