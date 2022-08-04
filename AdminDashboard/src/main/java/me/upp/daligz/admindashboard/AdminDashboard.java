04854f27d57f74df304f2f35f32945fb6f7ccd1c
Dali <oscardali370@gmail.com>
2022-08-04 00:41:27 -0500

    Connector instance created

--- AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/AdminDashboard.java
@@ -1,8 +1,10 @@
 package me.upp.daligz.admindashboard;
 
+import me.upp.daligz.admindashboard.database.Connector;
+
 public class AdminDashboard {
 
     public static void main(final String[] args) {
-
+        final Connector connector = new Connector();
     }
 }
