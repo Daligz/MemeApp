e8b36f4eff202790411255d0683d8c21e8fe1510
Dali <oscardali370@gmail.com>
2022-08-05 15:18:00 -0500

    Create implemented

---
+++ AdminDashboard/src/main/java/me/upp/daligz/admindashboard/controllers/ViewController.java
@@ -0,0 +1,23 @@
+package me.upp.daligz.admindashboard.controllers;
+
+import lombok.AllArgsConstructor;
+import me.upp.daligz.admindashboard.database.data.PostData;
+import me.upp.daligz.admindashboard.view.MainView;
+
+@AllArgsConstructor
+public class ViewController {
+
+    private final MainView mainView;
+    private final PostData postData;
+
+    public void init() {
+        this.create();
+    }
+
+    private void create() {
+        this.mainView.getBtnCreate().addActionListener(e -> this.postData.insert(
+                mainView.getTxtUrl().getText(),
+                mainView.getTxtCategory().getText()
+        ));
+    }
+}
