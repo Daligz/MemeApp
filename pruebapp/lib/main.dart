b278a8f5f6f2c53691b7e0f9f6978364f44c49be
Daligz <oscardali370@gmail.com>
2022-08-09 23:46:57 -0500

    Prompt lib added to get service ip

--- pruebapp/lib/main.dart
+++ pruebapp/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:prompt_dialog/prompt_dialog.dart';
 import 'package:pruebapp/sensor/authentication.dart';
+import 'package:pruebapp/service/service_routes.dart';
 import 'package:pruebapp/service/structures/users_structure.dart';
 import 'package:pruebapp/ventanas/home.dart';
 
@@ -11,7 +13,10 @@ void main() {
 class MyApp extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(final BuildContext context) {
+
+    // Dev mode
+    getIP(context);
 
     // Register device id
     UserStructure().insert();
@@ -27,5 +32,14 @@ class MyApp extends StatelessWidget {
 
   void checkAuth() async {
     if (!(await Authentication().hasPermission("Inicia sesion de forma segura"))) SystemNavigator.pop();
+  }
+
+  void getIP(final BuildContext context) async {
+    final String? ip = await prompt(context);
+    if (ip == null) {
+      getIP(context);
+    } else {
+      Routes.url = "http://$ip:6969";
+    }
   }
 }
--- pruebapp/lib/service/service_routes.dart
+++ pruebapp/lib/service/service_routes.dart
@@ -1,21 +1,21 @@
 class Routes {
 
   // Main url for the connection to the web service
-  static const String _url = "http://192.168.51.21:6969";
+  static String url = "http://192.168.1.105:6969";
 
   // Posts routes
-  static String routePostGet(final String category, final int amount) => _url + "/posts/get/$amount/$category";
-  static String routePostGetAll(final int amount) => _url + "/posts/get/$amount";
-  static String routePostGetReactions(final int postId) => _url + "/posts/reactions/$postId";
+  static String routePostGet(final String category, final int amount) => url + "/posts/get/$amount/$category";
+  static String routePostGetAll(final int amount) => url + "/posts/get/$amount";
+  static String routePostGetReactions(final int postId) => url + "/posts/reactions/$postId";
 
   // Users routes
-  static String routeUserInsert(final String mac) => _url + "/users/insert/$mac";
-  static String routeUserExists(final String mac) => _url + "/users/exists/$mac";
+  static String routeUserInsert(final String mac) => url + "/users/insert/$mac";
+  static String routeUserExists(final String mac) => url + "/users/exists/$mac";
 
   // Favorites routes
-  static String routeFavoriteInsert(final String mac, final String postId) => _url + "/fav/insert/$mac/$postId";
-  static String routeFavoriteDelete(final String mac, final String postId) => _url + "/fav/delete/$mac/$postId";
-  static String routeFavoriteGet(final String mac, final String postId) => _url + "/fav/get/$mac/$postId";
-  static String routeFavoriteExists(final String mac, final String postId) => _url + "/fav/exists/$mac/$postId";
-  static String routeFavoriteGetAll(final String mac) => _url + "/fav/get/$mac";
+  static String routeFavoriteInsert(final String mac, final String postId) => url + "/fav/insert/$mac/$postId";
+  static String routeFavoriteDelete(final String mac, final String postId) => url + "/fav/delete/$mac/$postId";
+  static String routeFavoriteGet(final String mac, final String postId) => url + "/fav/get/$mac/$postId";
+  static String routeFavoriteExists(final String mac, final String postId) => url + "/fav/exists/$mac/$postId";
+  static String routeFavoriteGetAll(final String mac) => url + "/fav/get/$mac";
 }
